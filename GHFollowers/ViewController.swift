57cde7e5105fd14f6b2deef9060e9cfb3eea4abc
Miguel Planckensteiner <49713385+mikeplanck@users.noreply.github.com>
2020-07-22 13:07:09 +0200

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Miguel Planckensteiner on 22.07.20.
+//  Copyright © 2020 Miguel Planckensteiner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
